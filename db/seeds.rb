6a691da2c1e4b732e0284b433a8ce5d7a6563944
Myriam Dubois <myriam.dubois@skema.edu>
2020-05-13 18:43:36 +0200

    working, nut design to improve, and nesting from into restaurant page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = Restaurant.create!([
+  { name: "Le Costaud des Batignolles", address:"rue brochant", category: "french"},
+  { name: "cucuza", address:"rue des moines", category:"italian"},
+  { name: "Irene et Bernard", address:"Rue de la Joncquière", category: "french" },
+  { name: "Chinese express", address:"boulevard Arago", category:"chinese"},
+  { name: "La Baraque à frites", address: "rue de Liège", category:"belgian"}
+])
